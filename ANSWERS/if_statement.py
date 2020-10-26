c26cfb8b9cbbc13dcc4511a63146416747a5268e
jstrickler <jstrickler@gmail.com>
2020-10-26 15:26:58 -0400

    Monday 3:30

---
+++ ANSWERS/if_statement.py
@@ -0,0 +1,42 @@
+
+value = 56
+
+if value > 75:
+    print("wallaby")
+    print("koala")
+    if value > 90:
+        print("WAHOOOOO")
+elif value > 50: # else if
+    print("wombat")
+    print("cane toad")
+else:
+    print("kangaroo")
+    print("platypus")
+
+print("Done")
+
+
+# X is false if
+# X is numeric and == 0
+# X has len and len(X) == 0
+# X is None
+# X is False
+
+# other things are true
+
+rating = 75
+
+print("Rating ", "passed" if rating > 80 else "failed")
+
+#  A ? B : C
+#  B if A else C
+
+#  == != > < >= <= is
+
+#  and or not
+
+
+
+
+
+
