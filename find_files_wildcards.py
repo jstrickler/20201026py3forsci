61c68547f11ddad2536233aa4d5d5f156a5d7c9a
jstrickler <jstrickler@gmail.com>
2020-10-27 11:18:49 -0400

    updated find_files_wildcards.py

--- find_files_wildcards.py
+++ find_files_wildcards.py
@@ -1,12 +1,15 @@
+import sys
 import os
 from glob import glob
 
-target_pattern = 'hello*'
+TARGET_FOLDER = sys.argv.pop(1)   # first arg on command line
+TARGET_PATTERN = sys.argv.pop(1)  # second arg on command line
+
 
 found = False
-for folder, subfolders, files in os.walk('/Users/jstrick/Desktop'):   #  C:/Users/yourname
+for folder, subfolders, files in os.walk(TARGET_FOLDER):   #  C:/Users/yourname, etc.
     os.chdir(folder)
-    target_matches = glob(target_pattern)
+    target_matches = glob(TARGET_PATTERN)
     for file_name in files:
         if file_name in target_matches:
             print(os.path.join(folder, file_name))
